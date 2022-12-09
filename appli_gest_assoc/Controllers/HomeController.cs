9371c12613fdc05cdf8a4a2e62baecfe11213a1d
Ousseini <ousseinijiko@gmail.com>
2022-12-09 00:27:04 +0000

    login

--- appli_gest_assoc/Controllers/HomeController.cs
+++ appli_gest_assoc/Controllers/HomeController.cs
@@ -1,16 +1,17 @@
 ﻿using appli_gest_assoc.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Data.SqlClient;
 using Microsoft.Extensions.Logging;
-using System;
-using System.Collections.Generic;
 using System.Diagnostics;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace appli_gest_assoc.Controllers
 {
     public class HomeController : Controller
     {
+        SqlConnection con = new SqlConnection();
+        SqlCommand com = new SqlCommand();
+        SqlDataReader dr;
+
         private readonly ILogger<HomeController> _logger;
 
         public HomeController(ILogger<HomeController> logger)
@@ -22,10 +23,36 @@ namespace appli_gest_assoc.Controllers
         {
             return View();
         }
-
+        
+        // Get Account
+        [HttpGet]
         public IActionResult Login()
         {
             return View();
+        }
+        void connectionString()
+        {
+            con.ConnectionString = "Data Source=localhost\\SQLEXPRESS; Initial Catalog=gest_assoc_db; integrated security=true";
+        }
+
+        [HttpPost]
+        public IActionResult Verify(Account acc)
+        {
+            connectionString();
+            con.Open();
+            com.Connection = con;
+            com.CommandText = "select * from login where email='"+acc.Email+"' and password='"+acc.password+"'";
+            dr = com.ExecuteReader();
+            if (dr.Read())
+            {
+                con.Close();
+                return View("Index");
+            }
+            else
+            {
+                con.Close();
+                return View("ErrorLogin");
+            }
         }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
